29ffdbcb7763452ef6e70264af7691ae9af9b0e2
Llego-250 <solideaze1251@gmail.com>
2026-04-22 14:22:03 +0200

    Update bottom navigation layout for Dashboard item and adjust styles

--- app/src/Main/java/com/example/appointment_schedule/AppointmentAdapter.java
+++ app/src/Main/java/com/example/appointment_schedule/AppointmentAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mugangaconnect.R;
 import com.google.android.material.button.MaterialButton;
 
 import java.util.List;
