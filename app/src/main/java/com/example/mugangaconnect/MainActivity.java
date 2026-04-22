1da555d68ee8e0c156a45e91168ebc7f7cb66537
Llego-250 <solideaze1251@gmail.com>
2026-04-22 13:36:50 +0200

    Remove unused imports from MainActivity.

--- app/src/main/java/com/example/mugangaconnect/MainActivity.java
+++ app/src/main/java/com/example/mugangaconnect/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.mugangaconnect;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
