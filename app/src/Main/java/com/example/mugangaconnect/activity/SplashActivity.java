946b0e8e95a509b6785628bf128b3d20ee1b7665
AngeKimberly <angekimberlym@gmail.com>
2026-04-27 13:34:31 +0200

    Updated LoginActivity import and routing in SplashActivity

--- app/src/Main/java/com/example/mugangaconnect/activity/SplashActivity.java
+++ app/src/Main/java/com/example/mugangaconnect/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.mugangaconnect.activity.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
