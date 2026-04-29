5790488a95cb573e456df6512f6a499067b71570
Llego-250 <solideaze1251@gmail.com>
2026-04-29 12:12:28 +0200

    Update the import in MainActivity to use the correct package path for BottomNavHelper

--- app/src/main/java/com/example/mugangaconnect/activity/MainActivity.java
+++ app/src/main/java/com/example/mugangaconnect/activity/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.mugangaconnect.BottomNavHelper;
+import com.example.mugangaconnect.activity.BottomNavHelper;
 import com.example.mugangaconnect.R;
 
 public class MainActivity extends AppCompatActivity {
