6ef8b1608bebcf7f561218bfb88a1ab550bca8ee
Llego-250 <solideaze1251@gmail.com>
2026-04-29 12:26:25 +0200

    Fix package declaration and add R import in SplashActivity

--- app/src/main/java/com/example/mugangaconnect/activity/SplashActivity.java
+++ app/src/main/java/com/example/mugangaconnect/activity/SplashActivity.java
@@ -1,10 +1,11 @@
-package com.example.mugangaconnect;
+package com.example.mugangaconnect.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.mugangaconnect.R;
 
 public class SplashActivity extends AppCompatActivity {
 
