b01f716bb0b38f410b47565a69195b12c01ebab6
Llego-250 <solideaze1251@gmail.com>
2026-05-11 20:16:47 +0200

    Re-add the missing TextView import that is still used in updateDoctorCard.

--- app/src/main/java/com/example/mugangaconnect/activity/AppointmentManagementActivity.java
+++ app/src/main/java/com/example/mugangaconnect/activity/AppointmentManagementActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
